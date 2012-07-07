e479cb543493c60c3e19749f648f60d8bafb056a
Thatcher Graham <tg@thatchergraham.com>
2012-07-06 23:34:23 -0400

    Changed resume link
    changed resume link on main page, added linkedin icon and link to resume
    page

---
+++ 060612_index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html> 
+
+<html>
+
+<head>
+<style type="text/css">
+	body {
+		color: white; 
+		background: black;
+}
+	div {
+		text-align: center; 
+		font-size: 100%;
+}
+	#container { 
+		margin: 200px auto;
+		width: 600px; 
+}	
+	#footer {
+		position:fixed;
+		bottom:0;
+		z-index:999999;
+		font-size:18px;
+}
+	img {
+	border:none
+}
+    a:link {
+    color:#FFFFFF;
+}
+    a:visited {
+    color:#FFFFFF;
+}
+    a:hover  {
+    opacity:0.5;
+    color:#0000FF;
+}
+
+</style>
+
+<title>ThatcherGraham.com</title>
+<meta name="keywords" content="Thatcher Graham, Arcane Radio Trivia, Jose Fritz, Zealous Fuel, The Radioman" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+     <link rel="shortcut icon" href="/favicon.ico">
+		<style type="text/css">
+        font-family: 'Droid Sans',Tahoma,Lucia Sans,Helvetica,Arial,Sans-Serif;
+		</style>
+		<script type="text/javascript">
+
+		<!--google analytics
+		-->
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29333375-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body>
+   
+<div id="container">
+	<a href="http://www.linkedin.com/in/thatchergraham" title="Resume"><img src="http://farm8.staticflickr.com/7197/6884653553_c9d0a0d9ed_s.jpg" alt="Resume"/></a>
+	<a href="mailto:tg-remove-this-so-I-know-you-are-not-a-spambot@thatchergraham.com" title="Email"><img src="http://farm8.staticflickr.com/7202/6878507609_ebc22ff004_s.jpg" alt="Email"/></a>
+	<a href="http://tenwatts.blogspot.com/" title="Blog"><img src="http://farm8.staticflickr.com/7199/6878507583_9185ee7be0_s.jpg" alt="Photography"/></a>
+	<a href="http://www.flickr.com/photos/75019839@N00/" title="Photography"><img src="http://farm8.staticflickr.com/7040/6878507527_8b286a728d_s.jpg" alt="Photography"/></a>
+	<a href="http://zealousfuel.bandcamp.com/" title="Music"><img src="http://farm8.staticflickr.com/7199/6878507553_87b54472a6_s.jpg" alt="Music"/></a>
+</div>
+
+<div id="footer">
+<a href="http://www.w3.org/html/logo/" title="HTML5 Valid"><img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-performance-semantics.png" width="98" height="32" alt="HTML5 Powered"></a>
+<a href="http://jigsaw.w3.org/css-validator/check/referer" title="CSS level 2.1"><img style="border:0;width:80px;height:30px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"></a>
+<a href="http://www.thatchergraham.com/resume.htm"><b>Resume in HTML</b><alt="Resume in HTML"/></a>
+</div>
+
+<!-- Old Links	 
+<a href="http://www.linkedin.com/in/thatchergraham">Resume</a> 
+<a href="http://www.flickr.com/photos/75019839@N00/">Photography</a> 
+<a href="http://tenwatts.blogspot.com/">Blog</a> 
+<a href="http://zealousfuel.bandcamp.com/">Music</a> 
+-->
+</body>
+
+</html> 
+
