24a77f1479db7327be90902de4040ad114520269
Thatcher Graham <tg@thatchergraham.com>
2012-12-31 13:27:48 -0500

    update

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html> 
+
+<html>
+
+<head>
+<style type="text/css">
+	body {
+		color: white; 
+		background-image:url('http://farm8.staticflickr.com/7059/6888941375_0837f8ec3e_o.gif') ;		
+}
+	div {
+		text-align: center; 
+		font-size: 100%;
+}
+	#wrapper { 
+		opacity:0.95;
+		margin: 100px auto;
+		margin-top: 70px;
+		margin-bottom: 50px;
+		width: 650px; 
+		padding: 75px;
+
+}
+	#container1 { 
+		width: 600px; 
+}	
+	#container2 { 
+		width: 600px; 
+}	
+	#container3 { 
+		width: 600px; 
+}	
+	#footer {
+		position:fixed;
+		bottom:0;
+		z-index:999999;
+		font-size:18px;
+}
+	img {
+	border:none
+}
+    a:link {
+    color:#FFFFFF;
+}
+    a:visited {
+    color:#FFFFFF;
+}
+    a:hover  {
+    opacity:0.5;
+    color:#0000FF;
+}
+
+</style>
+
+<title>ThatcherGraham.com</title>
+<meta name="keywords" content="Thatcher Graham, Arcane Radio Trivia, Jose Fritz, Zealous Fuel, The Radioman" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+     <link rel="shortcut icon" href="/favicon.ico">
+		<style type="text/css">
+        font-family: 'Droid Sans',Tahoma,Lucia Sans,Helvetica,Arial,Sans-Serif;
+		</style>
+		<script type="text/javascript">
+
+		<!--google analytics
+		-->
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29333375-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body>
+   
+
+<div id="wrapper">
+<div id="container1">
+	<a href="http://www.thatchergraham.com/resume.htm" title="Resume"><img src="http://farm8.staticflickr.com/7278/7524001798_7f8066d13d_o.jpg" width="75" height="75" alt="Resume"/></a>
+	<a href="#" title="Blank"><img src="http://farm9.staticflickr.com/8168/7523950878_0a23b9341a_o.jpg" width="75" height="75" alt="Blank"/></a>
+	<a href="mailto:tg-remove-this-so-I-know-you-are-not-a-spambot@thatchergraham.com" title="Email"><img src="http://farm9.staticflickr.com/8431/7524001912_d64d9f3d2e_o.jpg" width="75" height="75" alt="Email"/></a>
+</div>
+<div id="container2">
+	<a href="#" title="Blank"><img src="http://farm9.staticflickr.com/8168/7523950878_0a23b9341a_o.jpg" width="75" height="75" alt="Blank"/></a>
+	<a href="http://tenwatts.blogspot.com/" title="Blog"><img src="http://farm8.staticflickr.com/7133/7524001866_f0f32ab9ef_o.jpg" width="75" height="75" alt="Photography"/></a>
+	<a href="#" title="Blank"><img src="http://farm9.staticflickr.com/8168/7523950878_0a23b9341a_o.jpg" width="75" height="75" alt="Blank"/></a>
+</div>
+<div id="container1">
+	<a href="http://www.flickr.com/photos/75019839@N00/" title="Photography"><img src="http://farm8.staticflickr.com/7125/7524001684_b2c854f6c8_o.jpg" width="75" height="75" alt="Photography"/></a>
+	<a href="#" title="Blank"><img src="http://farm9.staticflickr.com/8168/7523950878_0a23b9341a_o.jpg" width="75" height="75" alt="Blank"/></a>
+	<a href="http://zealousfuel.bandcamp.com/" title="Music"><img src="http://farm9.staticflickr.com/8165/7524001746_2c4c01a673_o.jpg" width="75" height="75" alt="Music"/></a>
+</div>
+</div>
+	
+
+<div id="footer">
+<a href="http://www.w3.org/html/logo/" title="HTML5 Valid"><img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-performance-semantics.png" width="98" height="32" alt="HTML5 Powered"></a>
+<a href="http://jigsaw.w3.org/css-validator/check/referer" title="CSS level 2.1"><img style="border:0;width:80px;height:30px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"></a>
+</div>
+
+<!-- Old Links	 
+<a href="http://www.linkedin.com/in/thatchergraham">Resume</a> 
+<a href="http://www.flickr.com/photos/75019839@N00/">Photography</a> 
+<a href="http://tenwatts.blogspot.com/">Blog</a> 
+<a href="http://zealousfuel.bandcamp.com/">Music</a> 
+-->
+</body>
+
+</html> 
+
